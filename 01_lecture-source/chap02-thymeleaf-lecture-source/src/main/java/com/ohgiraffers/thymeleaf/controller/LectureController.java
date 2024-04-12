2a9c55cbcf1c4de28151e60b846b65e1eca65039
DESKTOP-DP3RJ5I\hi <sunnyskylove22@gmail.com>
2024-04-12 15:54:48 +0900

    추가

--- 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
+++ 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -1,16 +1,23 @@
 package com.ohgiraffers.thymeleaf.controller;
 
 import com.ohgiraffers.thymeleaf.model.dto.MemberDTO;
+import com.ohgiraffers.thymeleaf.model.dto.SelectCriteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
-@RequestMapping
+@RequestMapping("/lecture")
 public class LectureController {
 
-    @GetMapping("/lecture/expression")
+    @GetMapping("/expression")
     public ModelAndView expression(ModelAndView mv) {
 
         /* ModelAndView 이 친구는 Spring-Web-MVC 파트에서 깊게 다룰 예정이다.
@@ -35,6 +42,44 @@ public class LectureController {
         mv.addObject("num",1);
         mv.addObject("str","바나나");
 
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동",20,'남',"서울시 노원구"));
+        memberList.add(new MemberDTO("유관순",16,'여',"서울시 도봏구"));
+        memberList.add(new MemberDTO("장보고",40,'남',"서울시 노진구"));
+        memberList.add(new MemberDTO("신사임당",30,'여',"서울시 영구"));
+
+        mv.addObject("memberList",memberList);
+
         return mv;
     }
+
+    @GetMapping("/etc")
+    public ModelAndView etc(ModelAndView mv) {
+
+        SelectCriteria selectCriteria = new SelectCriteria(1,10,3);
+        mv.addObject(selectCriteria);
+
+        MemberDTO member = new MemberDTO("조평훈",20,'남',"서울시 노진구");            //  푸쉬~~~!!!!
+
+        mv.addObject("member", member);
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동",20,'남',"서울시 도봉구"));
+        memberList.add(new MemberDTO("유관순",16,'여',"서울시 노진구"));
+        memberList.add(new MemberDTO("장보고",30,'남',"서울시 길도봉구"));
+        memberList.add(new MemberDTO("신사임당",40,'여',"서울시 백구"));
+
+        mv.addObject("memberList",memberList);
+
+        Map<String, MemberDTO> memberMap = new HashMap<>();
+        memberMap.put("m01", new MemberDTO("홍길동", 20, '남', "서울시 도봉구"));
+        memberMap.put("m02", new MemberDTO("유관순", 16, '여', "서울시노진구"));
+        memberMap.put("m03", new MemberDTO("장보고", 30, '남', "서울시 길도봉구"));
+        memberMap.put("m04", new MemberDTO("신사임당", 40, '여', "서울시 백구"));
+
+        mv.addObject("memberMap", memberMap);
+
+        return mv;
+    }
+
 }
