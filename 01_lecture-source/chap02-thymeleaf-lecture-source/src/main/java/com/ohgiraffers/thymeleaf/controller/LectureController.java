48da5c9f0e71fdaeafa7ce942ec6ecbf6cf9542e
DESKTOP-DP3RJ5I\hi <sunnyskylove22@gmail.com>
2024-04-16 09:31:58 +0900

    수정테스트

--- 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
+++ 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -16,14 +16,14 @@ import java.util.Map;
 @Controller
 @RequestMapping("/lecture")
 public class LectureController {
- 
+
     @GetMapping("/expression")
     public ModelAndView expression(ModelAndView mv) {
 
         /* ModelAndView 이 친구는 Spring-Web-MVC 파트에서 깊게 다룰 예정이다.
          *  따라서 지금은 단순히 내가 보여줄 동적인 페이지와 정보들을 담을 클래스 라고
          *  생각을 해보자
-         *  */
+         *  */ 
 
         mv.addObject("member", new MemberDTO("조평훈",20,'남', "경기도 수원시"));
 
